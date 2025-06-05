8f94993f28401f0e2dbb3e61be227ade48e14a12
iKizuna <piotrjazwinski94@gazeta.pl>
2025-06-05 19:29:51 +0200

    added screen

--- main.py
+++ main.py
@@ -2,10 +2,19 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
 
-    print("Starting Asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        screen.fill((0, 0, 0))  # Fill screen with black
+        pygame.display.flip()   # Update the display
 if __name__ == "__main__":
     main()
