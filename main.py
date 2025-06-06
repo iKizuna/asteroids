c12708b50ab5d820b35ea9d14ebd5403493e72d6
iKizuna <piotrjazwinski94@gazeta.pl>
2025-06-06 10:16:29 +0200

    player movement done

--- main.py
+++ main.py
@@ -3,18 +3,33 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
 
+    dt = 0.0  # Delta time
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    # Create player at center of screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
+        dt = clock.tick(60) / 1000.0  # Get delta time in seconds
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
     
         screen.fill((0, 0, 0))  # Fill screen with black
+        player.draw(screen)  # Draw player
         pygame.display.flip()   # Update the display
+
+    pygame.time.Clock().tick(60)  # Limit to 60 FPS
+    pygame.quit()
+
 if __name__ == "__main__":
     main()
